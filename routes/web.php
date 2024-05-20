a01b281daa0f1dedc2ffa21631542eb04e0a3f36
Mustaaaaa <moustafaibrahim1512@gmail.com>
2024-05-20 17:39:21 +0200

    add:

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/photo', function () {
+    return view('photo');
+});
+Route::get('/text', function () {
+    return view('text');
 });
